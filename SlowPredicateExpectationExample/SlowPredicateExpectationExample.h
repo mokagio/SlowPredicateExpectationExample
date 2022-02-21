ecfca2df4474aac93f9ad2598c8a83bb71f59321
Gio Lodi <giovanni.lodi42@gmail.com>
2022-02-22 10:50:58 +1100

    Generate project with Xcode 13.2

---
+++ SlowPredicateExpectationExample/SlowPredicateExpectationExample.h
@@ -0,0 +1,18 @@
+//
+//  SlowPredicateExpectationExample.h
+//  SlowPredicateExpectationExample
+//
+//  Created by Gio on 22/2/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SlowPredicateExpectationExample.
+FOUNDATION_EXPORT double SlowPredicateExpectationExampleVersionNumber;
+
+//! Project version string for SlowPredicateExpectationExample.
+FOUNDATION_EXPORT const unsigned char SlowPredicateExpectationExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlowPredicateExpectationExample/PublicHeader.h>
+
+
